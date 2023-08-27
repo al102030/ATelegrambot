7788873377e4fdddab8661d9d6ad98b6969ce982
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-27 19:43:44 +0330

    Add response to main route.

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -4,7 +4,7 @@ from bot.Telegram import Telegram
 
 
 app = Flask(__name__, static_url_path='/static')
-app.config['SECRET_KEY'] = '876f18f346759c935f2d390e313edd93'
+# app.config['SECRET_KEY'] = '876f18f346759c935f2d390e313edd93'
 bot_methods = Telegram(TOKEN)
 
 # from flaskapp import routes  # noqa: E402
