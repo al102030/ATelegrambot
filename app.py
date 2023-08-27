75c12de6ae898a2b5cf9a08161671deea0fea4c5
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-27 18:56:58 +0330

    Add telegram method module and flask app

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ from config.secrets import HOST
 if __name__ == "__main__":
     bot_methods.remove_webhook()
     bot_methods.set_webhook(HOST)
-    app.run(debug=True, port=8000)
+    app.run(debug=True)
