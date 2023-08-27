dbba5dea4311f1cfeb8de3242b61c7b3faabf940
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-27 20:16:03 +0330

    check bot methods.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,10 +1,11 @@
 
 from flask import request, Response
-from flaskapp import app
+from flaskapp import app, bot_methods
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    bot_methods.send_message("msg", 112042461)
     if request.method == 'POST':
         print("A message received")
         return Response('ok', status=200)
