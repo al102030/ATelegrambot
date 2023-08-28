8e62a5cbec326a86099fce68b49a1da275d20762
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-28 23:45:21 +0330

    Add bot_method to get json and send message to my account.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,12 +1,13 @@
 
 from flask import request, Response
-from flaskapp import app
+from flaskapp import app, bot_methods
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == 'POST':
-        print("A message received")
+        msg = request.get_json()
+        bot_methods.send_message(msg, 112042461)
         return Response('ok', status=200)
     else:
         return '<h1>Asazoon Telegram Bot</h1>'
