46afe2597cbc9260f6f8e4f584f75dc2a2244b06
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-28 23:32:11 +0330

    Change Flask port.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -5,7 +5,6 @@ from flaskapp import app, bot_methods
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    bot_methods.send_message("msg", 112042461)
     if request.method == 'POST':
         print("A message received")
         return Response('ok', status=200)
