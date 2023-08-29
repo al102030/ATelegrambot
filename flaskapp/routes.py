f8f138746a7df5d8c88f3c9537599c8a7c6f9661
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-29 14:41:40 +0330

    To check Redirection and using other routes.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -8,16 +8,14 @@ def index():
     if request.method == 'POST':
         msg = request.get_json()
         if "start" in msg["message"]["text"]:
-            print("Start")
             # return redirect(url_for("token"))
-            token()
-        # return Response('ok', status=200)
+            token(msg)
+        return Response('ok', status=200)
     else:
         return render_template("home.html")
 
 
 @app.route("/token", methods=["GET", "POST"])
-def token():
+def token(msg):
     if request.method == 'POST':
-        bot_methods.send_message("it works!", 112042461)
-        return Response('ok', status=200)
+        bot_methods.send_message(msg, 112042461)
