8bc6c560569a2e63289b8429f6489dae22806283
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-29 21:00:01 +0330

    Send some Parameters using requests library to flask route.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -9,24 +9,25 @@ from flaskapp import app, bot_methods
 def index():
     if request.method == 'POST':
         msg = request.get_json()
-        try:
-            text = msg['message']['text']
-        except KeyError as error:
-            print("KeyError :", error)
-            text = None
-        if text:
-            if "/start" in msg["message"]["text"]:
-                chat_id = msg['message']['chat']['id']
-                headers = {
-                    "accept": "application/json",
-                    "content-type": "application/json"
-                }
-                params = {
-                    "chat_id": chat_id,
-                    "text": text
-                }
-                requests.get(f"{LOCALHOST}/token", params=params,
-                             headers=headers, timeout=5)
+        bot_methods.send_message(msg, 112042461)
+        # try:
+        #     text = msg['message']['text']
+        # except KeyError as error:
+        #     print("KeyError :", error)
+        #     text = None
+        # if text:
+        #     if "/start" in msg["message"]["text"]:
+        #         chat_id = msg['message']['chat']['id']
+        #         headers = {
+        #             "accept": "application/json",
+        #             "content-type": "application/json"
+        #         }
+        #         params = {
+        #             "chat_id": chat_id,
+        #             "text": text
+        #         }
+        #         requests.get(f"{LOCALHOST}/token", params=params,
+        #                      headers=headers, timeout=5)
         return Response('ok', status=200)
     else:
         return render_template("home.html")
