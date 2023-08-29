362234df8b4a7501122a8e3a467850f631481cc2
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-29 20:47:50 +0330

    Send some Parameters using requests library to flask route.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -17,13 +17,15 @@ def index():
         if text:
             if "/start" in msg["message"]["text"]:
                 chat_id = msg['message']['chat']['id']
-
+                headers = {
+                    "accept": "application/json",
+                    "content-type": "application/json"
+                }
                 params = {
                     "chat_id": chat_id,
                     "text": text
                 }
-                requests.get(f"{LOCALHOST}/token", params=params,
-                             headers=headers, timeout=5)
+                requests.get(f"{LOCALHOST}/token", params=params, timeout=5)
         return Response('ok', status=200)
     else:
         return render_template("home.html")
@@ -32,7 +34,7 @@ def index():
 @app.route("/token", methods=["GET", "POST"])
 def token():
     if request.method == 'GET':
-        # text = request.args.get('text')
-        # chat_id = request.args.get("chat_id")
-        bot_methods.send_message("ok", 112042461)
+        text = request.args.get('text')
+        chat_id = request.args.get("chat_id")
+        bot_methods.send_message(f"{text} from {chat_id}", 112042461)
         return "Message has been sent."
