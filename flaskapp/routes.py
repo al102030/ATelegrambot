db4852067a588f6dd95b3c01146d9c2b2ae218c2
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-29 20:39:01 +0330

    Send some Parameters using requests library to flask route.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -9,7 +9,6 @@ from flaskapp import app, bot_methods
 def index():
     if request.method == 'POST':
         msg = request.get_json()
-        # bot_methods.send_message(msg, 112042461)
         try:
             text = msg['message']['text']
         except KeyError as error:
@@ -18,15 +17,16 @@ def index():
         if text:
             if "/start" in msg["message"]["text"]:
                 chat_id = msg['message']['chat']['id']
-                # headers = {
-                #     "accept": "application/json",
-                #     "content-type": "application/json"
-                # }
+                headers = {
+                    "accept": "application/json",
+                    "content-type": "application/json"
+                }
                 params = {
                     "chat_id": chat_id,
                     "text": text
                 }
-                requests.get(f"{LOCALHOST}/token", params=params, timeout=5)
+                requests.get(f"{LOCALHOST}/token", params=params,
+                             headers=headers, timeout=5)
         return Response('ok', status=200)
     else:
         return render_template("home.html")
