1cb33305f85616d807c1f53bc96fad1e929d1f4b
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-29 23:59:12 +0330

    Send some Parameters using requests library to flask route.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -18,16 +18,8 @@ def index():
         bot_methods.send_message(chat_id, 112042461)
         if text is not None:
             if "/start" in text:
-                print(f"text is >>>> {text} and chat_id = >>> {chat_id}")
-                # headers = {
-                #     "accept": "application/json",
-                #     "content-type": "application/json"
-                # }
-                # params = {
-                #     "chat_id": chat_id,
-                #     "text": text
-                # }
-                requests.post(f"{LOCALHOST}/token", timeout=5)
+                requests.post(f"{LOCALHOST}/token", timeout=20)
+        bot_methods.send_message("Response was sent.", 112042461)
         return Response('ok', status=200)
     else:
         return render_template("home.html")
@@ -36,8 +28,18 @@ def index():
 @app.route("/token", methods=["GET", "POST"])
 def token():
     if request.method == 'POST':
+        bot_methods.send_message("token route", 112042461)
+        return "Message has been sent."
+
+        # headers = {
+        #     "accept": "application/json",
+        #     "content-type": "application/json"
+        # }
+        # params = {
+        #     "chat_id": chat_id,
+        #     "text": text
+        # }
+
         # text = request.args.get('text')
         # chat_id = request.args.get("chat_id")
         # f"{text} from {chat_id}"
-        bot_methods.send_message("token route", 112042461)
-        return "Message has been sent."
