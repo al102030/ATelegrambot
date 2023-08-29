2cae7dcc1d596cb8e0b47d2120510db2392554b0
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-29 11:42:01 +0330

    Add new route and make templates folder.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,5 +1,5 @@
 
-from flask import request, Response
+from flask import request, Response, render_template
 from flaskapp import app, bot_methods
 
 
@@ -10,4 +10,9 @@ def index():
         bot_methods.send_message(msg, 112042461)
         return Response('ok', status=200)
     else:
-        return '<h1>Asazoon Telegram Bot</h1>'
+        return render_template("home.html")
+
+
+@app.route("/token")
+def get_token():
+    pass
