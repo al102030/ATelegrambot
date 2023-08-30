86bb0e68fd568429510699c9e102142ae8cd47a3
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-30 12:23:20 +0330

    change in list_maker to get dictionary key and value.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -26,6 +26,7 @@ def index():
                 }
                 response = requests.post(
                     f"{LOCALHOST}/token", params=params, timeout=20)
+                print(type(response))
                 user_select_keyboard = list_maker(response)
             bot_methods.send_message_with_menu(
                 "Please select", chat_id, user_select_keyboard)
