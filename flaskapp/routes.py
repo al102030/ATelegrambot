c714b166537771e2e553eedfa146a2be7103ed5f
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-30 11:54:37 +0330

    Add fake server, list_maker, and check token methods to main route.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -15,15 +15,17 @@ def index():
         except KeyError as error:
             print("KeyError :", error)
             text = None
-        bot_methods.send_message(chat_id, 112042461)
         if text is not None:
             if "/start" in text:
                 params = {
                     "chat_id": chat_id,
                     "text": text
                 }
-                requests.post(f"{LOCALHOST}/token", params=params, timeout=20)
-        # bot_methods.send_message("Response was sent.", 112042461)
+                response = requests.post(
+                    f"{LOCALHOST}/token", params=params, timeout=20)
+            user_select_keyboard = list_maker(response)
+            bot_methods.send_message_with_menu(
+                "Please select", chat_id, user_select_keyboard)
         return Response('ok', status=200)
     else:
         return render_template("home.html")
@@ -32,7 +34,35 @@ def index():
 @app.route("/token", methods=["GET", "POST"])
 def token():
     if request.method == 'POST':
+        text = ((request.args.get('text')).strip()).replace("/start", "")
+        if text is not None:
+            params = {
+                "text": text,
+            }
+            response = requests.post(
+                f"{LOCALHOST}/server", params=params, timeout=20)
+        return response
+
+
+@app.route("/server", methods=["GET", "POST"])
+def server():
+    if request.method == 'POST':
         text = request.args.get('text')
-        chat_id = request.args.get('chat_id')
-        bot_methods.send_message(f"{text} from {chat_id}", 112042461)
-        return "Message has been sent."
+        if "e6fbd60e70962e97" in text:
+            return {1: "A", 2: "B", 3: "C", 4: "D"}
+        elif "4676de3ae0db1ea7" in text:
+            return {5: "E", 6: "F", 7: "G", 8: "H"}
+        else:
+            None
+
+
+def list_maker(server_json):
+    user_select_keyboard = []
+    for item in server_json:
+        lst = []
+        dictionary = {}
+        dictionary['text'] = item.key()
+        dictionary['callback_data'] = item.value()
+        lst.append(dictionary)
+        user_select_keyboard.append(lst)
+    return user_select_keyboard
