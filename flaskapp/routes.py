bed0a435d95f6be354a1d8cddeb76e86a6cf85c5
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-08-30 12:00:07 +0330

    Add fake server, list_maker, and check token methods to main route.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -23,7 +23,7 @@ def index():
                 }
                 response = requests.post(
                     f"{LOCALHOST}/token", params=params, timeout=20)
-            user_select_keyboard = list_maker(response)
+                user_select_keyboard = list_maker(response)
             bot_methods.send_message_with_menu(
                 "Please select", chat_id, user_select_keyboard)
         return Response('ok', status=200)
