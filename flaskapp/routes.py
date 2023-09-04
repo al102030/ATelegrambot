7b2f336744f7cf1a9e66f02717797fad07909398
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-09-04 09:27:14 +0330

    Ceheck menu call back data.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -30,7 +30,7 @@ def index():
                     f"{LOCALHOST}/token", params=params, timeout=20)
                 if response.text != "Not allowed!":
                     user_select_keyboard = list_maker(response.json())
-                    bot_methods.send_message_with_menu(
+                    bot_methods.send_message_with_keyboard(
                         "Please select", chat_id, user_select_keyboard)
                 else:
                     print("Wrong User!")
@@ -62,7 +62,7 @@ def token():
 @app.route("/server", methods=["GET", "POST"])
 def server():
     if request.method == 'POST':
-        json_string1 = json.dumps({"1": "A", "2": "B", "3": "C", "4": "Dddd"})
+        json_string1 = json.dumps({"1": "A", "2": "B", "3": "C", "4": "D"})
         json_string2 = json.dumps({"5": "E", "6": "F", "7": "G", "8": "H"})
         text = request.args.get('text')
         if "e6fbd60e70962e97" in text:
@@ -82,4 +82,5 @@ def list_maker(server_json):
         dictionary['callback_data'] = value
         lst.append(dictionary)
         user_select_keyboard.append(lst)
+        print(user_select_keyboard)
     return user_select_keyboard
